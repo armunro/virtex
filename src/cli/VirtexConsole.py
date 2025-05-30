2c5115fdb06ed31d591a52cf1d79ce3748f2e36a
Andrew Munro <12575339+armunro@users.noreply.github.com>
2025-05-29 19:57:54 -0600

    Fixed imports

--- src/cli/VirtexConsole.py
+++ src/cli/VirtexConsole.py
@@ -1,8 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../" ))
-from VTXHid import Keys
-from VTXHid import Virtext
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../common" ))
+import Keys
+import Virtext
 
 def show_console():
     print("Enter your input (press Ctrl+D to exit):")
