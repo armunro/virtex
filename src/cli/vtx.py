5920cb21f21be0175d30c4f4cf1a8093580d9a8c
Andrew Munro <12575339+armunro@users.noreply.github.com>
2025-08-25 14:37:20 -0600

    Add echo

--- src/cli/vtx.py
+++ src/cli/vtx.py
@@ -15,6 +15,10 @@ parser = argparse.ArgumentParser(description="Virtex")
 subparsers = parser.add_subparsers(dest='command', help='Subcommands')
 
 # Update
+parse_echo = subparsers.add_parser('echo', help='Type text to the target')
+parse_echo.add_argument('text')
+
+# Update
 parse_update = subparsers.add_parser('update', help='Retrieve the latest copy of Virtex')
 # Cat
 parse_vtxt = subparsers.add_parser('cat', help='Echo a text file to the VHID.')
@@ -40,5 +44,7 @@ elif args.command == 'run':
     Virtext.execute_step_file(args.file)
 elif args.command == 'cat':
     Virtext.send_file(args.file)
+elif args.command == 'echo':
+    Virtext.send_file(args.text)
 else:
     parser.print_help()
