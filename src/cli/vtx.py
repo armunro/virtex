f5aa28321284f55705d4db4083528ac47c68b3ca
Andrew Munro <12575339+armunro@users.noreply.github.com>
2025-08-25 14:47:59 -0600

    Fixes

--- src/cli/vtx.py
+++ src/cli/vtx.py
@@ -16,18 +16,25 @@ import Mouse
 parser = argparse.ArgumentParser(description="Virtex")
 subparsers = parser.add_subparsers(dest='command', help='Subcommands')
 
-# Update
+# Echo
 parse_echo = subparsers.add_parser('echo', help='Type text to the target')
 parse_echo.add_argument('text')
 
+# Launch
+parse_launch = subparsers.add_parser('launch', help='Open an application or file using the run dialog.')
+parse_launch.add_argument('path')
+
 # Update
 parse_update = subparsers.add_parser('update', help='Retrieve the latest copy of Virtex')
+
 # Cat
 parse_vtxt = subparsers.add_parser('cat', help='Echo a text file to the VHID.')
 parse_vtxt.add_argument('file')
+
 # Vtxt
 parse_vtxt = subparsers.add_parser('run', help='Replay HID automation files remotely.')
 parse_vtxt.add_argument('file')
+
 # Console
 parse_console = subparsers.add_parser('console', help='Interactive terminal with remote text entry.')
 
@@ -48,5 +55,7 @@ elif args.command == 'cat':
     Virtext.send_file(args.file)
 elif args.command == 'echo':
     Keys.type_string(args.text)
+elif args.command == 'launch':
+    Keys.launch_app(args.path)
 else:
     parser.print_help()
